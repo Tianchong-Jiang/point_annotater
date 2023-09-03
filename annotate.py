90fff89d94e5e93f686467dbcc84035db4117a1e
Tianchong Jiang <tianchongjiang@gmail.com>
2023-09-03 15:28:32 -0500

    annoatation script working

--- annotate.py
+++ annotate.py
@@ -2,12 +2,23 @@ import cv2
 import os
 import time
 import numpy as np
+import json
 
 class Annotator:
-    def __init__(self):
+    def __init__(self, basedir):
+        self.double_click_threshold = 0.2
+
+        self.img = None
+        self.curr_idx = 0
         self.clicked_points = []
         self.click_times = []
-        self.double_click_threshold = 0.2
+
+        self.images = []
+        self.anno_dict = {}
+
+        self.basedir = basedir
+
+        self.read_images(basedir)
 
     def click_event(self, event, x, y, flags, params):
         """Handles mouse click events."""
@@ -18,12 +29,10 @@ class Annotator:
             # Save the point coordinates when double clicked
             if len(self.click_times) > 0 and time.time() - self.click_times[-1] < self.double_click_threshold:
 
-                self.update_image()
-
                 cv2.circle(self.img, (x, y), 7, (255, 255, 255), -1)
 
                 # show prompt
-                cv2.putText(self.img, 'Enter number:', (500, 500), cv2.FONT_HERSHEY_SIMPLEX, 5, (0, 0, 255), 10)
+                cv2.putText(self.img, 'Enter number:', (100, 500), cv2.FONT_HERSHEY_SIMPLEX, 5, (0, 0, 255), 20)
 
                 cv2.imshow('image', self.img)
 
@@ -50,6 +59,15 @@ class Annotator:
                 self.click_times.pop()
                 self.update_image()
 
+    # read all images in a directory
+    def read_images(self, path):
+        for filename in os.listdir(path):
+            if filename.endswith('.jpg') or filename.endswith('.png'):
+                img = cv2.imread(os.path.join(path, filename))
+                if img is not None:
+                    self.images.append(img)
+                    self.anno_dict[filename] = None
+
     # check repeated points
     def check_repeated_points(self, n):
         for point in self.clicked_points:
@@ -59,27 +77,22 @@ class Annotator:
 
     # update image with self.clicked_points
     def update_image(self):
-        self.img = self.orig_img.copy()
+        self.img = self.images[self.curr_idx].copy()
+        cv2.putText(self.img, f'Image {self.curr_idx+1}/{len(self.images)}', (100, 250), cv2.FONT_HERSHEY_SIMPLEX, 5, (0, 0, 255), 20)
         for point in self.clicked_points:
-            cv2.circle(self.img, (point[0], point[1]), 7, (255, 255, 255), -1)
-            cv2.putText(self.img, str(point[2]), (point[0], point[1]), cv2.FONT_HERSHEY_SIMPLEX, 5, (255, 255, 255), 10)
+            cv2.circle(self.img, (point[0], point[1]), 10, (255, 255, 255), -1)
+            cv2.putText(self.img, str(point[2]), (point[0], point[1]), cv2.FONT_HERSHEY_SIMPLEX, 5, (255, 255, 255), 20)
         cv2.imshow('image', self.img)
 
-    def save_annotated_points(self, image_path, points):
-        """Saves the annotated points to a text file."""
-        annotation_file_path = os.path.splitext(image_path)[0] + '_annotations.txt'
-        with open(annotation_file_path, 'w') as f:
-            for point in points:
-                f.write(f"{point[0]}, {point[1]}, {point[2]}\n")
+    # save annotated points to a json file
+    def save_annotated_points(self):
+        with open(os.path.join(self.basedir, 'annotations.json'), 'w') as f:
+            json.dump(self.anno_dict, f)
 
-    def run(self, image_path):
-        global img
-        # Read the image
-        self.orig_img = cv2.imread(image_path)
-        self.img = self.orig_img.copy()
-
-        # Create a window
-        cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+    # annotate a single image
+    def annotate_image(self):
+        self.img = self.images[self.curr_idx].copy()
+        self.clicked_points = []
 
         # Set the callback function for mouse click events
         cv2.setMouseCallback('image', self.click_event)
@@ -88,22 +101,35 @@ class Annotator:
         cv2.resizeWindow('image', 800, 600)
 
         # Display the image
-        cv2.imshow('image', self.img)
+        self.update_image()
 
-        # Wait for q key to exit
+        # wait for annotation
         while True:
-            key = cv2.waitKey(1) & 0xFF
+            key = cv2.waitKey(0) & 0xFF
             if key == ord('q'):
+                exit()
+            elif key == ord('n'):
+                print("Next image")
                 break
 
-        # Save the annotated points to a text file
-        self.save_annotated_points(image_path, self.clicked_points)
+        self.anno_dict[list(self.anno_dict.keys())[self.curr_idx]] = self.clicked_points
 
-        # Destroy all windows
+    # annotate all images in the directory
+    def annotate_all(self):
+        # Create a window
+        cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+
+        # Annotate each image
+        for i, image in enumerate(self.images):
+            print(f"Annotating image {i+1}/{len(self.images)}")
+            self.curr_idx = i
+            self.annotate_image()
+
+        # Save the annotated points to a text file
+        self.save_annotated_points()
+
         cv2.destroyAllWindows()
 
 if __name__ == '__main__':
-    # Replace 'image.jpg' with the path to the image you want to annotate.
-    image_path = 'image.jpg'
-    annotator = Annotator()
-    annotator.run(image_path)
+    annotator = Annotator("images/")
+    annotator.annotate_all()
